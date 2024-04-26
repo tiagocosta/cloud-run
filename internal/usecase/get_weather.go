c859ff8050aa84dd3a884cdd443586d10f2d1f80
tiagocs <tiago.c.silva@planejamento.gov.br>
2024-04-26 09:12:04 -0300

    mocking api tests

--- internal/usecase/get_weather.go
+++ internal/usecase/get_weather.go
@@ -1,14 +1,12 @@
 package usecase
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
 
 	"github.com/tiagocosta/cloud-run/configs"
 	"github.com/tiagocosta/cloud-run/internal/entity"
+	"github.com/tiagocosta/cloud-run/internal/pkg/wheatherapi"
+	"github.com/tiagocosta/cloud-run/internal/pkg/zipcodeapi"
 )
 
 var cfg configs.Conf
@@ -26,35 +24,6 @@ type GetWeatherOutputDTO struct {
 type GetWeatherUseCase struct {
 }
 
-type ViaCEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Unidade     string `json:"unidade"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-}
-
-type WeatherAPI struct {
-	Location struct {
-		Name           string  `json:"name"`
-		Region         string  `json:"region"`
-		Country        string  `json:"country"`
-		Lat            float64 `json:"lat"`
-		Lon            float64 `json:"lon"`
-		TzID           string  `json:"tz_id"`
-		LocaltimeEpoch int     `json:"localtime_epoch"`
-		Localtime      string  `json:"localtime"`
-	} `json:"location"`
-	Current struct {
-		TempC     float64  `json:"temp_c"`
-		Condition struct{} `json:"condition"`
-	} `json:"current"`
-}
-
 func NewGetWeatherUseCase() *GetWeatherUseCase {
 	cfg = configs.LoadConfig[configs.Conf](".")
 	return &GetWeatherUseCase{}
@@ -65,13 +34,13 @@ func (uc *GetWeatherUseCase) Execute(input GetWeatherInputDTO) (GetWeatherOutput
 	if err != nil {
 		return GetWeatherOutputDTO{}, err
 	}
-	viaCEP, err := FindZipCode(weather.Zip)
+	city, err := zipcodeapi.FindCity(weather.Zip)
 	if err != nil {
 		return GetWeatherOutputDTO{}, err
 	}
 
-	weather.City = viaCEP.Localidade
-	celsius, err := FetchWeather(weather.City)
+	weather.City = city
+	celsius, err := wheatherapi.GetWeather(weather.City, cfg.WeatherAPIKey)
 	if err != nil {
 		return GetWeatherOutputDTO{}, err
 	}
@@ -84,45 +53,4 @@ func (uc *GetWeatherUseCase) Execute(input GetWeatherInputDTO) (GetWeatherOutput
 	}
 
 	return out, nil
-}
-
-func FindZipCode(zipCode string) (*ViaCEP, error) {
-	resp, err := http.Get("https://viacep.com.br/ws/" + zipCode + "/json/")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var c ViaCEP
-	err = json.Unmarshal(body, &c)
-	if err != nil {
-		return nil, err
-	}
-
-	return &c, nil
-}
-
-func FetchWeather(city string) (float64, error) {
-	url := "https://api.weatherapi.com/v1/current.json?q=" + url.PathEscape(city) + "&key=" + cfg.WeatherAPIKey
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-
-	var w WeatherAPI
-	err = json.NewDecoder(resp.Body).Decode(&w)
-	if err != nil {
-		return 0, err
-	}
-
-	if (WeatherAPI{}) == w {
-		return 0, fmt.Errorf("can not find zipcode")
-	}
-
-	return w.Current.TempC, nil
 }
--- internal/usecase/get_weather_test.go
+++ internal/usecase/get_weather_test.go
@@ -4,112 +4,67 @@ import (
 	"errors"
 	"reflect"
 	"testing"
-
-	"github.com/stretchr/testify/mock"
 )
 
-type WeatherAPIMocked struct {
-	mock.Mock
-}
+func TestGetWeatherUseCase_Execute(t *testing.T) {
+	mock := new(GetWeatherUseCaseMocked)
+	mock.On("Execute", GetWeatherInputDTO{
+		ZipCode: "71218010",
+	}).Return(GetWeatherOutputDTO{
+		Celsius:    "25.60",
+		Fahrenheit: "78.08",
+		Kelvin:     "298.60",
+	}, nil)
+	mock.On("Execute", GetWeatherInputDTO{
+		ZipCode: "12345678",
+	}).Return(GetWeatherOutputDTO{}, errors.New("can not find zipcode"))
 
-func (m *WeatherAPIMocked) FetchWeather(city string) (float64, error) {
-	args := m.Called(city)
-	return args.Get(0).(float64), args.Error(1)
-}
-
-func TestFindZipCode(t *testing.T) {
 	type args struct {
-		zipCode string
+		input GetWeatherInputDTO
 	}
 	tests := []struct {
 		name    string
+		uc      *GetWeatherUseCase
 		args    args
-		want    *ViaCEP
+		want    GetWeatherOutputDTO
 		wantErr bool
 	}{
 		{
-			"Invalid zipcode (length less than 8). Should return an error",
+			"Valid inputDTO. Should return all temperatures in outputDTO",
+			&GetWeatherUseCase{},
 			args{
-				zipCode: "1234567",
+				GetWeatherInputDTO{
+					ZipCode: "71218010",
+				},
 			},
-			nil,
+			GetWeatherOutputDTO{
+				Celsius:    "25.60",
+				Fahrenheit: "78.08",
+				Kelvin:     "298.60",
+			},
+			false,
+		},
+		{
+			"Non existing zipcode. Should return an error",
+			&GetWeatherUseCase{},
+			args{
+				GetWeatherInputDTO{
+					ZipCode: "12345678",
+				},
+			},
+			GetWeatherOutputDTO{},
 			true,
-		},
-		{
-			"Valid but non existing zipcode. Should return an empty struct",
-			args{
-				zipCode: "12345678",
-			},
-			&ViaCEP{},
-			false,
-		},
-		{
-			"Valid and existing zipcode. Should return an fully populated struct",
-			args{
-				zipCode: "71218010",
-			},
-			&ViaCEP{
-				Cep:         "71218-010",
-				Logradouro:  "SMAS Trecho 1 C",
-				Complemento: "(Condomínio Living SQPS)",
-				Bairro:      "Zona Industrial (Guará)",
-				Localidade:  "Brasília",
-				Uf:          "DF",
-				Ibge:        "5300108",
-				Gia:         "",
-			},
-			false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := FindZipCode(tt.args.zipCode)
+			got, err := mock.Execute(tt.args.input)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("FindZipCode() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("GetWeatherUseCase.Execute() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("FindZipCode() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestFetchWeather(t *testing.T) {
-	mock := new(WeatherAPIMocked)
-	mock.On("FetchWeather", "Brasília").Return(29.0, nil)
-	mock.On("FetchWeather", "non existing city").Return(0.0, errors.New("can not find zipcode"))
-	type args struct {
-		city string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    float64
-		wantErr bool
-	}{
-		{
-			"Valid City. Should return the correct temperature",
-			args{city: "Brasília"},
-			29.0,
-			false,
-		},
-		{
-			"Invalid City. Should return error",
-			args{city: "non existing city"},
-			0.0,
-			true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := mock.FetchWeather(tt.args.city)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("FetchWeather() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("FetchWeather() = %v, want %v", got, tt.want)
+				t.Errorf("GetWeatherUseCase.Execute() = %v, want %v", got, tt.want)
 			}
 		})
 	}
